95309bd1d984189963c555a520c89635b66893eb
1285723569xb <baiwenlei+19971209>
2021-07-28 15:35:13 +0800

    幼儿保健

--- src/main/java/com/cykj/controller/Health.java
+++ src/main/java/com/cykj/controller/Health.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -29,7 +30,7 @@ public class Health {
 
     @GetMapping("/HealthAll")
     public @ResponseBody
-    String GGB2(int page, String uaccount) {
+    String GGB2(@RequestParam("page") int page, @RequestParam("uaccount") String uaccount) {
         System.out.println("保健信息进来了");
         System.out.println(page);
         System.out.println(uaccount);
@@ -39,4 +40,24 @@ public class Health {
         System.out.println(s);
         return s;
     }
+    
+    /**
+     * @Description: 小程序幼儿保健
+     * @Param: 
+     * @Author: BWL
+     * @Date: 2021-07-28 15:34
+     */
+    @ResponseBody
+    @GetMapping("/selHeaUphone")
+    public String selHeaUphone(@RequestParam("page") int page, @RequestParam("uphone") String uphone) {
+        System.out.println("手机保健信息进来了");
+        System.out.println(page);
+        System.out.println(uphone);
+        List<Healthbean> health = healthMapper.selHeaUphone(uphone, (page - 1) * 5, page * 5);
+        Gson gson = new Gson();
+        String s = gson.toJson(health);
+        System.out.println(s);
+        return s;
+    }
+
 }
