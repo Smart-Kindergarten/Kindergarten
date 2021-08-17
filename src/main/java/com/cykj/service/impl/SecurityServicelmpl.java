125157700c92cd17dcaa4697008adaa94de502b4
1131473932 <87411414+1131473932@users.noreply.github.com>
2021-08-17 18:20:07 +0800

    CZX

--- src/main/java/com/cykj/service/impl/SecurityServicelmpl.java
+++ src/main/java/com/cykj/service/impl/SecurityServicelmpl.java
@@ -14,7 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;   
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
